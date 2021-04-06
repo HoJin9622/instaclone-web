e08ba204c49be4087dc49ffcf20eaa6c110640bf
HoJin9622 <kiss0104040@gmail.com>
2021-04-06 20:16:14 +0900

    8.3 Router Setup part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path='/potato'>
+            <h1>Potato</h1>
+          </Route>
+          <Route path='/banana'>
+            <h1>Banana</h1>
+          </Route>
+          <Route path='/banana/is/delicious'>
+            <h1>Banana is delicious</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
