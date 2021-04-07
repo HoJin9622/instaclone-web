71d500ea16b9ec1c7a0fbd5871560dc7eeb8d86e
HoJin9622 <kiss0104040@gmail.com>
2021-04-07 22:29:55 +0900

    10.2 Cleaning Login Code

--- src/styles.ts
+++ src/styles.ts
@@ -2,18 +2,27 @@ import reset from 'styled-reset'
 import { createGlobalStyle, DefaultTheme } from 'styled-components'
 
 export const lightTheme: DefaultTheme = {
-  fontColor: '#2c2c2c',
-  bgColor: 'lightgray',
+  accent: '#0095f6',
+  borderColor: 'rgb(219, 219, 219)',
 }
 
-export const darkTheme: DefaultTheme = {
-  fontColor: 'lightgray',
-  bgColor: '#2c2c2c',
-}
+export const darkTheme: DefaultTheme = {}
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
+    input {
+      all: unset;
+    }
+    * {
+      box-sizing: border-box;
+    }
     body {
-        background-color: ${(props) => props.theme.bgColor};
+        background-color: #fafafa;
+        font-size: 14px;
+        font-family: 'Open Sans', sans-serif;
+        color: rgba(38, 38, 38);
+    }
+    a {
+      text-decoration: none;
     }
 `
