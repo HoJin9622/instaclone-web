dbfafcd42bd5f08e62337eb9fd30ff0137b9a8e1
HoJin9622 <kiss0104040@gmail.com>
2021-04-11 02:01:34 +0900

    10.12 Create Account

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,9 @@
+import { useMutation } from '@apollo/client'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import gql from 'graphql-tag'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useHistory } from 'react-router'
 import styled from 'styled-components'
 import AuthLayout from '../components/auth/AuthLayout'
 import BottomBox from '../components/auth/BottomBox'
@@ -9,6 +13,10 @@ import Input from '../components/auth/Input'
 import PageTitle from '../components/PageTitle'
 import { FatLink } from '../components/shared'
 import routes from '../routes'
+import {
+  createAccount,
+  createAccountVariables,
+} from '../__generated__/createAccount'
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -22,7 +30,68 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `
 
+interface IForm {
+  firstName: string
+  lastName?: string
+  email: string
+  username: string
+  password: string
+}
+
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $email: String!
+    $firstName: String!
+    $lastName: String
+    $password: String!
+    $username: String!
+  ) {
+    createAccount(
+      email: $email
+      firstName: $firstName
+      lastName: $lastName
+      password: $password
+      username: $username
+    ) {
+      ok
+      error
+    }
+  }
+`
+
 const SignUp = () => {
+  const history = useHistory()
+
+  const onCompleted = (data: createAccount) => {
+    const {
+      createAccount: { ok, error },
+    } = data
+    if (!ok) {
+      return
+    }
+    history.push(routes.home)
+  }
+  const [createAccount, { loading }] = useMutation<
+    createAccount,
+    createAccountVariables
+  >(CREATE_ACCOUNT_MUTATION, { onCompleted })
+
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState: { isValid },
+  } = useForm({
+    mode: 'onChange',
+  })
+
+  const onSubmitValid: SubmitHandler<IForm> = (data) => {
+    if (loading) {
+      return
+    }
+    createAccount({ variables: { ...data } })
+  }
+
   return (
     <AuthLayout>
       <PageTitle title='Sign up' />
@@ -33,12 +102,42 @@ const SignUp = () => {
             Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type='text' placeholder='Email' />
-          <Input type='text' placeholder='Name' />
-          <Input type='text' placeholder='Username' />
-          <Input type='password' placeholder='Password' />
-          <Button type='submit' value='Log in' />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({ required: 'First Name is reuiqred.' })}
+            name='firstName'
+            type='text'
+            placeholder='First Name'
+          />
+          <Input
+            ref={register}
+            name='lastName'
+            type='text'
+            placeholder='Last Name'
+          />
+          <Input
+            ref={register({ required: 'Email  is reuiqred.' })}
+            name='email'
+            type='text'
+            placeholder='Email'
+          />
+          <Input
+            ref={register({ required: 'Username  is reuiqred.' })}
+            name='username'
+            type='text'
+            placeholder='Username'
+          />
+          <Input
+            ref={register({ required: 'Password  is reuiqred.' })}
+            name='password'
+            type='password'
+            placeholder='Password'
+          />
+          <Button
+            type='submit'
+            value={loading ? 'Loading' : 'Sign up'}
+            disabled={!isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta='Have an account?' linkText='Log in' link={routes.home} />
