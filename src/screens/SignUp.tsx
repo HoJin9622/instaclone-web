6a99302508caaa2a0c7431bb157e95486a0c388f
HoJin9622 <kiss0104040@gmail.com>
2021-04-11 02:22:08 +0900

    Add Validation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -9,6 +9,7 @@ import AuthLayout from '../components/auth/AuthLayout'
 import BottomBox from '../components/auth/BottomBox'
 import Button from '../components/auth/Button'
 import FormBox from '../components/auth/FormBox'
+import FormError from '../components/auth/FormError'
 import Input from '../components/auth/Input'
 import PageTitle from '../components/PageTitle'
 import { FatLink } from '../components/shared'
@@ -36,6 +37,7 @@ interface IForm {
   email: string
   username: string
   password: string
+  result?: string
 }
 
 const CREATE_ACCOUNT_MUTATION = gql`
@@ -68,7 +70,7 @@ const SignUp = () => {
       createAccount: { ok, error },
     } = data
     if (!ok) {
-      return
+      return setError('result', { message: error || undefined })
     }
     history.push(routes.home, {
       message: 'Account created. Please log in.',
@@ -87,7 +89,8 @@ const SignUp = () => {
     errors,
     formState: { isValid },
     getValues,
-  } = useForm({
+    setError,
+  } = useForm<IForm>({
     mode: 'onChange',
   })
 
@@ -114,7 +117,9 @@ const SignUp = () => {
             name='firstName'
             type='text'
             placeholder='First Name'
+            hasError={Boolean(errors.firstName?.message)}
           />
+          <FormError message={errors.firstName?.message} />
           <Input
             ref={register}
             name='lastName'
@@ -122,28 +127,35 @@ const SignUp = () => {
             placeholder='Last Name'
           />
           <Input
-            ref={register({ required: 'Email  is reuiqred.' })}
+            ref={register({ required: 'Email is reuiqred.' })}
             name='email'
             type='text'
             placeholder='Email'
+            hasError={Boolean(errors.email?.message)}
           />
+          <FormError message={errors.email?.message} />
           <Input
-            ref={register({ required: 'Username  is reuiqred.' })}
+            ref={register({ required: 'Username is reuiqred.' })}
             name='username'
             type='text'
             placeholder='Username'
+            hasError={Boolean(errors.username?.message)}
           />
+          <FormError message={errors.username?.message} />
           <Input
-            ref={register({ required: 'Password  is reuiqred.' })}
+            ref={register({ required: 'Password is reuiqred.' })}
             name='password'
             type='password'
             placeholder='Password'
+            hasError={Boolean(errors.password?.message)}
           />
+          <FormError message={errors.password?.message} />
           <Button
             type='submit'
             value={loading ? 'Loading' : 'Sign up'}
             disabled={!isValid || loading}
           />
+          <FormError message={errors.result?.message} />
         </form>
       </FormBox>
       <BottomBox cta='Have an account?' linkText='Log in' link={routes.home} />
