e8f384a0ce21b379b29768827f214a4f598b0012
HoJin9622 <kiss0104040@gmail.com>
2021-04-12 22:29:51 +0900

    11.18 Create Comment part Three

--- src/components/feed/Comments.tsx
+++ src/components/feed/Comments.tsx
@@ -77,11 +77,27 @@ const Comments: VFC<IProps> = ({
           ...userData.me,
         },
       }
+      const newCacheComment = cache.writeFragment({
+        data: newComment,
+        fragment: gql`
+          fragment BSName on Comment {
+            id
+            createdAt
+            isMine
+            payload
+            user {
+              username
+              avatar
+            }
+          }
+        `,
+      })
+      console.log(newCacheComment)
       cache.modify({
         id: `Photo:${photoId}`,
         fields: {
           comments(prev) {
-            return [...prev, newComment]
+            return [...prev, newCacheComment]
           },
           commentNumber(prev) {
             return prev + 1
