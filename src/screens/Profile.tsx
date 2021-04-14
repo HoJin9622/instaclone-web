5db38d0913d57020939a3bfbfa02cb35724e21ce
HoJin9622 <kiss0104040@gmail.com>
2021-04-14 20:40:17 +0900

    12.1 Queries and Fragments

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,29 @@
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
 import { useParams } from 'react-router'
+import { PHOTO_FRAGMENT } from '../fragment'
+import { seeProfile, seeProfileVariables } from '../__generated__/seeProfile'
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      id
+      firstName
+      lastName
+      username
+      bio
+      avatar
+      photos {
+        ...PhotoFragment
+      }
+      totalFollowing
+      totalFollowers
+      isMe
+      isFollowing
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`
 
 interface IParams {
   username: string
@@ -6,6 +31,12 @@ interface IParams {
 
 const Profile = () => {
   const { username } = useParams<IParams>()
+  const { data } = useQuery<seeProfile, seeProfileVariables>(
+    SEE_PROFILE_QUERY,
+    { variables: { username } }
+  )
+
+  console.log(data)
 
   return <div></div>
 }
